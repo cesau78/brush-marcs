9b516bdb8504eb28313fc875cf3f7b9ba3945ece
Charles Esau <charles.esau@jfdi.co>
2024-09-11 07:19:09 -0400

    Added okta integration for logging in

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
+import "./LoginButton";
+import LoginButton from "./LoginButton";
 
 function App() {
   return (
     <div className="App">
+      <LoginButton />
       <header className="App-header">Brush mArcs</header>
     </div>
   );
